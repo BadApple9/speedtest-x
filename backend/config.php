e6170c0689a10f6e737957c5528c21e1e0dfa323
HuJK <hu@whojk.com>
2020-12-20 23:25:59 +0800

    chart

--- backend/config.php
+++ backend/config.php
@@ -3,7 +3,7 @@
 /**
  * 最多保存多少条测试记录
  */
-const MAX_LOG_COUNT = 100;
+const MAX_LOG_COUNT = 100000;
 
 /**
  * IP运营商解析服务：(1) ip.sb | (2) ipinfo.io （如果1解析ip异常，请切换成2）
@@ -13,4 +13,4 @@ const IP_SERVICE = 'ip.sb';
 /**
  * 是否允许同一IP记录多条测速结果
  */
-const SAME_IP_MULTI_LOGS = false;
+const SAME_IP_MULTI_LOGS = true;
